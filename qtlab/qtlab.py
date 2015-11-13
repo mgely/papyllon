a5a901d24ced9b87061c8eaf15b48979e3f9ebea
macTUD <m.a.cohen@tudelft.nl>
2015-11-13 16:55:46 +0100

    retrieved qtlab kernel communication file
    Next:
    -remove print warnings
    - communicate with the kernerl, maybe with
    http://jupyter-client.readthedocs.org/en/latest/api/client.html

--- qtlab/qtlab.py
+++ qtlab/qtlab.py
@@ -1,19 +1,20 @@
 import IPython 
 from  multiprocessing import Process
+import os
 import time
 
 def start_kernel():
-    IPython.start_kernel(file_to_run='source/qtlab_shell.py')
+    IPython.start_kernel(file_to_run='source/qtlab_shell.py', connection_file='kernel.json')
 
 def setup_communication_file():
     pass
 
 def start_ipython():
-    IPython.start_ipython(argv=['console','--existing'])
+    print str('--existing '+os.path.dirname(os.path.realpath(__file__))+'\kernel.json')
+    IPython.start_ipython(argv=['console','--existing',os.path.dirname(os.path.realpath(__file__))+'\kernel.json'])
 
 if __name__ == "__main__":
     Process(target=start_kernel).start()
-    time.sleep(1)
+    time.sleep(2)
 
-    setup_communication_file()
     start_ipython()
